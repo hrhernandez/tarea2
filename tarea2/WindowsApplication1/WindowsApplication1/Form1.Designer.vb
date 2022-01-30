de2059651b071d6053e57af4bbea75c0010068c9
hrhernandez <98706148+hrhernandez@users.noreply.github.com>
2022-01-30 11:42:11 -0600

    Add files via upload

---
+++ tarea2/WindowsApplication1/WindowsApplication1/Form1.Designer.vb
@@ -0,0 +1,311 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.txtazucar = New System.Windows.Forms.TextBox()
+        Me.txtfrijol = New System.Windows.Forms.TextBox()
+        Me.txtarroz = New System.Windows.Forms.TextBox()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.GroupBox2 = New System.Windows.Forms.GroupBox()
+        Me.Label8 = New System.Windows.Forms.Label()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.GroupBox3 = New System.Windows.Forms.GroupBox()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.txtsub = New System.Windows.Forms.TextBox()
+        Me.txtiva = New System.Windows.Forms.TextBox()
+        Me.txtparcial = New System.Windows.Forms.TextBox()
+        Me.txtdesc = New System.Windows.Forms.TextBox()
+        Me.txttotal = New System.Windows.Forms.TextBox()
+        Me.GroupBox1.SuspendLayout()
+        Me.GroupBox2.SuspendLayout()
+        Me.GroupBox3.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.BackColor = System.Drawing.Color.Moccasin
+        Me.GroupBox1.Controls.Add(Me.txtazucar)
+        Me.GroupBox1.Controls.Add(Me.txtfrijol)
+        Me.GroupBox1.Controls.Add(Me.txtarroz)
+        Me.GroupBox1.Controls.Add(Me.Label3)
+        Me.GroupBox1.Controls.Add(Me.Label2)
+        Me.GroupBox1.Controls.Add(Me.Label1)
+        Me.GroupBox1.Location = New System.Drawing.Point(15, 12)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(290, 206)
+        Me.GroupBox1.TabIndex = 0
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "INGRESO DE DATOS"
+        '
+        'txtazucar
+        '
+        Me.txtazucar.Location = New System.Drawing.Point(104, 161)
+        Me.txtazucar.Name = "txtazucar"
+        Me.txtazucar.Size = New System.Drawing.Size(154, 20)
+        Me.txtazucar.TabIndex = 5
+        Me.txtazucar.Text = "0"
+        '
+        'txtfrijol
+        '
+        Me.txtfrijol.Location = New System.Drawing.Point(104, 91)
+        Me.txtfrijol.Name = "txtfrijol"
+        Me.txtfrijol.Size = New System.Drawing.Size(154, 20)
+        Me.txtfrijol.TabIndex = 4
+        Me.txtfrijol.Text = "0"
+        '
+        'txtarroz
+        '
+        Me.txtarroz.Location = New System.Drawing.Point(104, 32)
+        Me.txtarroz.Name = "txtarroz"
+        Me.txtarroz.Size = New System.Drawing.Size(154, 20)
+        Me.txtarroz.TabIndex = 3
+        Me.txtarroz.Text = "0"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(12, 161)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(86, 13)
+        Me.Label3.TabIndex = 2
+        Me.Label3.Text = "Libras de Azúcar"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(12, 91)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(74, 13)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "Libras de Frijol"
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(12, 32)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(77, 13)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Libras de Arroz"
+        '
+        'GroupBox2
+        '
+        Me.GroupBox2.BackColor = System.Drawing.Color.Moccasin
+        Me.GroupBox2.Controls.Add(Me.txttotal)
+        Me.GroupBox2.Controls.Add(Me.txtdesc)
+        Me.GroupBox2.Controls.Add(Me.txtparcial)
+        Me.GroupBox2.Controls.Add(Me.txtiva)
+        Me.GroupBox2.Controls.Add(Me.txtsub)
+        Me.GroupBox2.Controls.Add(Me.Label8)
+        Me.GroupBox2.Controls.Add(Me.Label7)
+        Me.GroupBox2.Controls.Add(Me.Label6)
+        Me.GroupBox2.Controls.Add(Me.Label5)
+        Me.GroupBox2.Controls.Add(Me.Label4)
+        Me.GroupBox2.Location = New System.Drawing.Point(323, 12)
+        Me.GroupBox2.Name = "GroupBox2"
+        Me.GroupBox2.Size = New System.Drawing.Size(416, 205)
+        Me.GroupBox2.TabIndex = 1
+        Me.GroupBox2.TabStop = False
+        Me.GroupBox2.Text = "RESULTADOS"
+        '
+        'Label8
+        '
+        Me.Label8.AutoSize = True
+        Me.Label8.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label8.ForeColor = System.Drawing.Color.Red
+        Me.Label8.Location = New System.Drawing.Point(218, 23)
+        Me.Label8.Name = "Label8"
+        Me.Label8.Size = New System.Drawing.Size(123, 20)
+        Me.Label8.TabIndex = 7
+        Me.Label8.Text = "DESCUENTO:"
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.Font = New System.Drawing.Font("Microsoft Sans Serif", 36.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label7.Location = New System.Drawing.Point(213, 75)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(197, 55)
+        Me.Label7.TabIndex = 6
+        Me.Label7.Text = "TOTAL:"
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Location = New System.Drawing.Point(12, 161)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(73, 13)
+        Me.Label6.TabIndex = 5
+        Me.Label6.Text = "Pago Parcial: "
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(47, 91)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(27, 13)
+        Me.Label5.TabIndex = 4
+        Me.Label5.Text = "IVA:"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(26, 32)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(59, 13)
+        Me.Label4.TabIndex = 3
+        Me.Label4.Text = "Sub-Total :"
+        '
+        'GroupBox3
+        '
+        Me.GroupBox3.BackColor = System.Drawing.Color.Moccasin
+        Me.GroupBox3.Controls.Add(Me.Button3)
+        Me.GroupBox3.Controls.Add(Me.Button2)
+        Me.GroupBox3.Controls.Add(Me.Button1)
+        Me.GroupBox3.Location = New System.Drawing.Point(159, 243)
+        Me.GroupBox3.Name = "GroupBox3"
+        Me.GroupBox3.Size = New System.Drawing.Size(467, 65)
+        Me.GroupBox3.TabIndex = 2
+        Me.GroupBox3.TabStop = False
+        '
+        'Button3
+        '
+        Me.Button3.Location = New System.Drawing.Point(324, 24)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(119, 24)
+        Me.Button3.TabIndex = 2
+        Me.Button3.Text = "SALIR"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(183, 24)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(119, 24)
+        Me.Button2.TabIndex = 1
+        Me.Button2.Text = "LIMPIAR"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(39, 24)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(119, 24)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "ACEPTAR"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'txtsub
+        '
+        Me.txtsub.Enabled = False
+        Me.txtsub.Location = New System.Drawing.Point(91, 29)
+        Me.txtsub.Name = "txtsub"
+        Me.txtsub.Size = New System.Drawing.Size(75, 20)
+        Me.txtsub.TabIndex = 6
+        '
+        'txtiva
+        '
+        Me.txtiva.Enabled = False
+        Me.txtiva.Location = New System.Drawing.Point(91, 91)
+        Me.txtiva.Name = "txtiva"
+        Me.txtiva.Size = New System.Drawing.Size(75, 20)
+        Me.txtiva.TabIndex = 13
+        '
+        'txtparcial
+        '
+        Me.txtparcial.Enabled = False
+        Me.txtparcial.Location = New System.Drawing.Point(91, 158)
+        Me.txtparcial.Name = "txtparcial"
+        Me.txtparcial.Size = New System.Drawing.Size(75, 20)
+        Me.txtparcial.TabIndex = 14
+        '
+        'txtdesc
+        '
+        Me.txtdesc.Enabled = False
+        Me.txtdesc.ForeColor = System.Drawing.Color.Red
+        Me.txtdesc.Location = New System.Drawing.Point(341, 23)
+        Me.txtdesc.Name = "txtdesc"
+        Me.txtdesc.Size = New System.Drawing.Size(59, 20)
+        Me.txtdesc.TabIndex = 15
+        '
+        'txttotal
+        '
+        Me.txttotal.Enabled = False
+        Me.txttotal.Font = New System.Drawing.Font("Microsoft Sans Serif", 24.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txttotal.ForeColor = System.Drawing.Color.Black
+        Me.txttotal.Location = New System.Drawing.Point(223, 133)
+        Me.txttotal.Name = "txttotal"
+        Me.txttotal.Size = New System.Drawing.Size(177, 44)
+        Me.txttotal.TabIndex = 16
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.SystemColors.ActiveCaption
+        Me.ClientSize = New System.Drawing.Size(787, 333)
+        Me.Controls.Add(Me.GroupBox3)
+        Me.Controls.Add(Me.GroupBox2)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
+        Me.GroupBox2.ResumeLayout(False)
+        Me.GroupBox2.PerformLayout()
+        Me.GroupBox3.ResumeLayout(False)
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents GroupBox1 As System.Windows.Forms.GroupBox
+    Friend WithEvents txtazucar As System.Windows.Forms.TextBox
+    Friend WithEvents txtfrijol As System.Windows.Forms.TextBox
+    Friend WithEvents txtarroz As System.Windows.Forms.TextBox
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents GroupBox2 As System.Windows.Forms.GroupBox
+    Friend WithEvents Label8 As System.Windows.Forms.Label
+    Friend WithEvents Label7 As System.Windows.Forms.Label
+    Friend WithEvents Label6 As System.Windows.Forms.Label
+    Friend WithEvents Label5 As System.Windows.Forms.Label
+    Friend WithEvents Label4 As System.Windows.Forms.Label
+    Friend WithEvents GroupBox3 As System.Windows.Forms.GroupBox
+    Friend WithEvents Button3 As System.Windows.Forms.Button
+    Friend WithEvents Button2 As System.Windows.Forms.Button
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents txtsub As System.Windows.Forms.TextBox
+    Friend WithEvents txttotal As System.Windows.Forms.TextBox
+    Friend WithEvents txtdesc As System.Windows.Forms.TextBox
+    Friend WithEvents txtparcial As System.Windows.Forms.TextBox
+    Friend WithEvents txtiva As System.Windows.Forms.TextBox
+
+End Class
---
+++ tarea2/WindowsApplication1/WindowsApplication1/Form1.vb
@@ -0,0 +1,75 @@
+﻿
+
+Public Class Form1
+
+    Const parroz As Double = 2.0
+    Const pfrijol As Double = 1.75
+    Const pazucar As Double = 2.5
+    Const iva As Double = 0.12
+    Const des As Double = 0.025
+
+    Dim subt As Double
+    Dim piva As Double
+    Dim pagop As Double
+    Dim d As Double
+    Dim totalt As Double
+
+    Private Sub txtarroz_KeyPress(ByVal sender As Object, ByVal e As System.Windows.Forms.KeyPressEventArgs) Handles txtarroz.KeyPress
+        If Not IsNumeric(e.KeyChar) Then
+            e.Handled = True
+        End If
+    End Sub
+
+    Private Sub Button3_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button3.Click
+        If MsgBox("¿Desea salir?", vbQuestion + vbYesNo, "AVISO") = vbYes Then
+            End
+        End If
+    End Sub
+
+
+    Private Sub txtfrijol_KeyPress(ByVal sender As Object, ByVal e As System.Windows.Forms.KeyPressEventArgs) Handles txtfrijol.KeyPress
+        If Not IsNumeric(e.KeyChar) Then
+            e.Handled = True
+        End If
+    End Sub
+
+
+    Private Sub txtazucar_KeyPress(ByVal sender As Object, ByVal e As System.Windows.Forms.KeyPressEventArgs) Handles txtazucar.KeyPress
+        If Not IsNumeric(e.KeyChar) Then
+            e.Handled = True
+        End If
+    End Sub
+
+    Private Sub Button2_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button2.Click
+        txtarroz.Clear()
+        txtfrijol.Clear()
+        txtazucar.Clear()
+        txtsub.Clear()
+        txtiva.Clear()
+        txtparcial.Clear()
+        txtdesc.Clear()
+        txttotal.Clear()
+    End Sub
+
+
+
+    Private Sub txtsub_TextChanged(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles txtsub.TextChanged
+
+    End Sub
+
+    Private Sub Button1_Click_1(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click
+
+        subt = (Val(txtarroz.Text) * parroz) + (Val(txtfrijol.Text) * pfrijol) + (Val(txtazucar.Text) * pazucar)
+        piva = subt * iva
+        pagop = subt + iva
+        d = pagop * des
+        totalt = pagop - d
+
+        txtsub.Text = subt
+        txtiva.Text = piva
+        txtparcial.Text = pagop
+        txtdesc.Text = d
+        txttotal.Text = totalt
+
+    End Sub
+End Class
